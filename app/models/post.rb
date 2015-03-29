3f73c3abb03cfb6f8f63c518ee0f21eee3ad3936
alex <melnikovsansan@gmail.com>
2015-03-29 15:14:59 +0300

    fix Post.published scope

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,7 @@ class Post < ActiveRecord::Base
   belongs_to :user
   validates_presence_of :published_at, :title, :content
 
-  scope :published, -> { where 'posts.published_at < date(\'now\')' }
+  scope :published, -> { where 'posts.published_at < current_timestamp' }
 
   acts_as_commentable
 end
